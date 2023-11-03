eda35c443ece7f835bc005a433a5bd58b8d8e46b
NaveenLapeace <naveenranasinghe1235@gmail.com>
2023-11-03 21:54:14 +0530

    fb config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCcIYnvU9TXmvRW4MdWU_o8TMf0OU7wFxo",
+    authDomain: "project-vr-mall.firebaseapp.com",
+    projectId: "project-vr-mall",
+    storageBucket: "project-vr-mall.appspot.com",
+    messagingSenderId: "650079548346",
+    appId: "1:650079548346:web:1d60e846e73fecc0c2a383"
+  }
 };
